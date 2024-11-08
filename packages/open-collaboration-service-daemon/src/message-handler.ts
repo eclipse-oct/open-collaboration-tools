ace57059fb193cf6186ac25c0226457bd44ff25f
Jonah Iden <jonah.iden@typefox.io>
2024-11-08 15:27:20 +0100

    restructuring and join handling
    Signed-off-by: Jonah Iden <jonah.iden@typefox.io>

--- packages/open-collaboration-service-daemon/src/message-handler.ts
+++ packages/open-collaboration-service-daemon/src/message-handler.ts
@@ -4,12 +4,11 @@
 // terms of the MIT License, which is available in the project root.
 // ******************************************************************************
 
-import { ConnectionProvider, Deferred, DisposableCollection, MaybePromise, ProtocolBroadcastConnection } from 'open-collaboration-protocol';
+import type * as types from 'open-collaboration-protocol';
+import { Deferred, MaybePromise } from 'open-collaboration-protocol';
 import { StdioCommunicationHandler } from './communication-handler';
-import { FromDaeomonMessage, JoinRoomRequest, LoginResponse, SendBroadcast, SendNotification, SendRequest, SendResponse, SessionCreated, ToDaemonMessage } from './messages';
-import * as Y from 'yjs';
-import * as awarenessProtocol from 'y-protocols/awareness';
-import { OpenCollaborationYjsProvider } from 'open-collaboration-yjs';
+import { CreateRoomRequest, FromDaeomonMessage, JoinRoomRequest, LoginResponse, SendBroadcast, SendNotification, SendRequest, SendResponse, SessionCreated, ToDaemonMessage } from './messages';
+import { CollaborationInstance } from './collaboration-instance';
 
 export class MessageHandler {
 
@@ -19,28 +18,32 @@ export class MessageHandler {
         [
             ['login', () => this.login()],
             ['join-room', message => this.joinRoom(message as JoinRoomRequest)],
-            ['create-room', () => this.createRoom()],
+            ['create-room', message => this.createRoom(message as CreateRoomRequest)],
             // when connection is established
             ['send-request', async message => (await this.sendRequest(message as SendRequest)).request],
             ['send-response', message => { this.openRequests.get((message as SendResponse).id)?.resolve((message as SendResponse).response); }],
-            ['send-broadcast', message => this.currentConnection?.sendBroadcast((message as SendBroadcast).broadcast.type, (message as SendBroadcast).broadcast.parameters)],
-            ['send-notification', message => this.currentConnection?.sendNotification((message as SendNotification).notification.type, (message as SendNotification).notification.parameters)],
-            ['leave-session',  () => this.currentConnection?.dispose()]
+            ['send-broadcast', message => this.currentCollaborationInstance?.currentConnection.sendBroadcast((message as SendBroadcast).broadcast.type, (message as SendBroadcast).broadcast.parameters)],
+            ['send-notification', message => this.currentCollaborationInstance?.currentConnection.sendNotification((message as SendNotification).notification.type, (message as SendNotification).notification.parameters)],
+            ['leave-session',  () => this.currentCollaborationInstance?.currentConnection.dispose()]
         ]
     );
 
-    protected currentConnection?: ProtocolBroadcastConnection;
-    protected yjsProvider?: OpenCollaborationYjsProvider;
-
-    protected connectionDisposables: DisposableCollection = new DisposableCollection();
+    protected currentCollaborationInstance?: CollaborationInstance;
 
     protected lastRequestId = 0;
 
-    constructor(private connectionProvider: ConnectionProvider, private communcationHandler: StdioCommunicationHandler) {
+    constructor(private connectionProvider: types.ConnectionProvider, private communcationHandler: StdioCommunicationHandler) {
         communcationHandler.onMessage(async message => {
-            const resp = await this.handlers.get(message.kind)?.(message);
-            if (resp) {
-                this.communcationHandler.sendMessage(resp);
+            try {
+                const resp = await this.handlers.get(message.kind)?.(message);
+                if (resp) {
+                    this.communcationHandler.sendMessage(resp);
+                }
+            } catch (error: any) {
+                communcationHandler.sendMessage({
+                    kind: 'error',
+                    message: error?.message
+                });
             }
         });
     }
@@ -55,7 +58,7 @@ export class MessageHandler {
 
     async joinRoom(message: JoinRoomRequest): Promise<SessionCreated> {
         const resp = await this.connectionProvider.joinRoom({ roomId: message.room});
-        this.onConnection(await this.connectionProvider.connect(resp.roomToken, resp.host));
+        this.onConnection(await this.connectionProvider.connect(resp.roomToken), false);
         return {
             kind: 'session',
             info: {
@@ -65,9 +68,9 @@ export class MessageHandler {
         };
     }
 
-    async createRoom(): Promise<SessionCreated> {
+    async createRoom(message: CreateRoomRequest): Promise<SessionCreated> {
         const resp = await this.connectionProvider.createRoom({});
-        this.onConnection(await this.connectionProvider.connect(resp.roomToken));
+        this.onConnection(await this.connectionProvider.connect(resp.roomToken), true, message.workspace);
         return {
             kind: 'session',
             info: {
@@ -77,68 +80,15 @@ export class MessageHandler {
         };
     }
 
-    onConnection(connection: ProtocolBroadcastConnection) {
-        this.currentConnection = connection;
-        const YjsDoc = new Y.Doc();
-        const awareness = new awarenessProtocol.Awareness(YjsDoc);
-        this.connectionDisposables.push({
-            dispose: () => {
-                YjsDoc.destroy();
-                awareness.destroy();
-            }});
-
-        this.yjsProvider = new OpenCollaborationYjsProvider(connection, YjsDoc, awareness);
-        this.yjsProvider.connect();
-        this.connectionDisposables.push(connection.onReconnect(() => {
-            this.yjsProvider?.connect();
-        }));
-
-        connection.onDisconnect(() => {
-            this.dispose();
-        });
-
-        connection.onUnhandledRequest(async (origin, method, ...parameters) => {
-            const id = this.lastRequestId++;
-            this.communcationHandler.sendMessage({
-                kind: 'on-request',
-                id,
-                request: {
-                    origin,
-                    method,
-                    parameters
-                }
-            });
-            const deferred = new Deferred<unknown>();
-            this.openRequests.set(id, deferred);
-            const res = await deferred.promise;
-            return res;
-        });
-
-        connection.onUnhandledNotification((origin, method, ...parameters) => {
-            this.communcationHandler.sendMessage({
-                kind: 'on-notification',
-                notification: {
-                    origin,
-                    method,
-                    parameters
-                }
-            });
-        });
-
-        connection.onUnhandledBroadcast((origin, method, ...parameters) => {
-            this.communcationHandler.sendMessage({
-                kind: 'on-broadcast',
-                broadcast: {
-                    origin,
-                    method,
-                    parameters
-                }
-            });
-        });
+    onConnection(connection: types.ProtocolBroadcastConnection, host: boolean, workspace?: types.Workspace) {
+        this.currentCollaborationInstance?.dispose();
+        this.currentCollaborationInstance = new CollaborationInstance(connection, host, workspace);
+        this.currentCollaborationInstance.onSendMessage(async message => this.communcationHandler.sendMessage(message));
+        this.currentCollaborationInstance.onSendRequest(async message => this.sendAndWaitForClient(message));
     }
 
     async sendRequest(message: SendRequest) {
-        const resp = await this.currentConnection?.sendRequest(message.request.type, message.request.parameters);
+        const resp = await this.currentCollaborationInstance?.currentConnection?.sendRequest(message.request.type, message.request.parameters);
         return {
             kind: 'response',
             request: resp,
@@ -146,9 +96,16 @@ export class MessageHandler {
         };
     }
 
+    async sendAndWaitForClient(messsage: any): Promise<unknown> {
+        const id = this.lastRequestId++;
+        messsage.id = id;
+        this.communcationHandler.sendMessage(messsage);
+        const deferred = new Deferred<unknown>();
+        this.openRequests.set(id, deferred);
+        return await deferred.promise;
+    }
+
     dispose() {
-        this.currentConnection?.dispose();
-        this.yjsProvider?.dispose();
-        this.connectionDisposables.dispose();
+        this.currentCollaborationInstance?.dispose();
     }
 }
