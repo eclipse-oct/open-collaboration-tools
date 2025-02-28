73bfcd7c2e69159c4038a35ceaeb286033dadf98
Jonah Iden <jonah.iden@typefox.io>
2025-02-28 10:24:36 +0100

    using mjs and import instead of just js
    Signed-off-by: Jonah Iden <jonah.iden@typefox.io>

--- packages/open-collaboration-service-process/scripts/sea-build.mjs
+++ packages/open-collaboration-service-process/scripts/sea-build.mjs
@@ -1,6 +1,6 @@
-const execSync = require('child_process').execSync;
-const fs = require('fs');
-const inject = require('postject').inject
+import { execSync } from 'child_process';
+import fs from 'fs';
+import { inject } from 'postject'
 
 var EXECUTABLE_NAME = 'oct-servcice-daemon'
 
@@ -13,14 +13,14 @@ if (process.platform === 'win32') {
 
 const postjectOptions = { sentinelFuse: 'NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2' }
 
-if(process.platform === 'darwin') {
+if (process.platform === 'darwin') {
     execSync(`codesign --remove-signature ${EXECUTABLE_NAME}`)
     postjectOptions.machoSegmentName = 'NODE_SEA'
 }
 
-console.log('injecting ', process.cwd()  + '/bin/sea-prep.blob', 'into ', process.cwd() + '/' + EXECUTABLE_NAME)
-inject(process.cwd() + '/bin/' + EXECUTABLE_NAME, 'NODE_SEA_BLOB', fs.readFileSync(process.cwd()  + '/bin/sea-prep.blob'), postjectOptions)
+console.log('injecting ', process.cwd() + '/bin/sea-prep.blob', 'into ', process.cwd() + '/' + EXECUTABLE_NAME)
+inject(process.cwd() + '/bin/' + EXECUTABLE_NAME, 'NODE_SEA_BLOB', fs.readFileSync(process.cwd() + '/bin/sea-prep.blob'), postjectOptions)
 
-if(process.platform === 'darwin') {
+if (process.platform === 'darwin') {
     execSync(`codesign --sign - ${EXECUTABLE_NAME}`)
 }
