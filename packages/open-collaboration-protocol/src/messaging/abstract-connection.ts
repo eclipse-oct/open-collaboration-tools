a4eb880dad8ff3e96e9e95d4a85a4006d3015544
Kai Salmen <kai.salmen@typefox.io>
2025-03-26 18:01:28 +0100

    Fix build order and version mismatchs

--- packages/open-collaboration-protocol/src/messaging/abstract-connection.ts
+++ packages/open-collaboration-protocol/src/messaging/abstract-connection.ts
@@ -262,7 +262,7 @@ export abstract class AbstractBroadcastConnection implements BroadcastConnection
     protected abstract getPublicKeysLength(): number;
 
     private async write(message: msg.Message): Promise<void> {
-        await this.options.transport.write(Encoding.encode(message));
+        await this.options.transport.write(Encoding.encode(message).buffer as ArrayBuffer);
     }
 
     onRequest(type: string, handler: Handler<any[], any>): void;
