4fee72d6449e1f75dfa1b71e9f59ef2ff7b06e90
Jonah Iden <jonah.iden@typefox.io>
2025-03-28 16:49:27 +0100

    basic updating and highlighting in editor
    Signed-off-by: Jonah Iden <jonah.iden@typefox.io>

--- packages/open-collaboration-intelliJ/src/main/kotlin/org/typefox/oct/CollaborationInstance.kt
+++ packages/open-collaboration-intelliJ/src/main/kotlin/org/typefox/oct/CollaborationInstance.kt
@@ -1,47 +1,42 @@
 package org.typefox.oct
 
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.EditorFactory
-import com.intellij.openapi.editor.event.DocumentEvent
-import com.intellij.openapi.editor.event.DocumentListener
-import com.intellij.openapi.editor.event.EditorFactoryEvent
-import com.intellij.openapi.editor.event.EditorFactoryListener
 import com.intellij.openapi.project.Project
-import org.eclipse.lsp4j.jsonrpc.services.JsonNotification
-import org.jetbrains.plugins.notebooks.visualization.addEditorDocumentListener
-import org.typefox.oct.editor.EditorListener
+import org.typefox.oct.editor.EditorManager
 import org.typefox.oct.fileSystem.WorkspaceFileSystemService
 
 
-class CollaborationInstance(val octService: OCTMessageHandler.OCTService, project: Project ): Disposable {
+class CollaborationInstance(octService: OCTMessageHandler.OCTService, project: Project) : Disposable {
 
-  val workspaceFileSystem: WorkspaceFileSystemService = project.getService(WorkspaceFileSystemService::class.java)
+    val workspaceFileSystem: WorkspaceFileSystemService = project.getService(WorkspaceFileSystemService::class.java)
 
-  val guests: ArrayList<Peer> = ArrayList()
-  var host: Peer? = null
+    private val guests: ArrayList<Peer> = ArrayList()
+    private var host: Peer? = null
 
-  init {
-    EditorFactory.getInstance().addEditorFactoryListener(EditorListener(octService), this)
-    println("initialized collaboration instance")
-  }
+    private val editorManager: EditorManager = EditorManager(octService, project)
 
-  fun updateTextSelection(url: String, selections: Array<ClientTextSelection>) {
-    println("update Text Selection $url")
-  }
+    init {
+        EditorFactory.getInstance().addEditorFactoryListener(editorManager, this)
+        println("initialized collaboration instance")
+    }
 
-  fun updateDocument(url: String, updates: Array<TextDocumentInsert>) {
-    println("update Document $url")
-  }
+    fun updateTextSelection(url: String, selections: Array<ClientTextSelection>) {
+        editorManager.updateTextSelection(url, selections)
+    }
 
-  fun initPeers(initData: InitData) {
-    guests.addAll(initData.guests)
-    host = initData.host
-  }
+    fun updateDocument(url: String, updates: Array<TextDocumentInsert>) {
+        editorManager.updateDocument(url, updates)
+    }
 
-  override fun dispose() {
+    fun initPeers(initData: InitData) {
+        guests.addAll(initData.guests)
+        host = initData.host
+    }
 
-  }
+    override fun dispose() {
+
+    }
 }
 
 
